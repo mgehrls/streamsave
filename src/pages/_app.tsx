4ae532de7a1e985cf9a5b87d6861bacaf5997b17
Matt Gehrls <mattgehrls@gmail.com>
2023-11-09 16:39:33 -0500

    font / layout fixes / logo upgrade

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Outfit } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const outfit = Outfit({ subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <div className={outfit.className}>
+        <Component {...pageProps} />
+      </div>
     </ClerkProvider>
   );
 };
