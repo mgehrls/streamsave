b3d3d84549f8845c0d7f949f0f30a6965c9ba5b0
Matt Gehrls <mattgehrls@gmail.com>
2023-11-28 14:47:10 -0500

    menu ux

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import type { UserResource } from "@clerk/types/dist/user";
+import { useState } from "react";
 
 export default function LayoutWrapper({
   children,
@@ -20,11 +21,16 @@ export default function LayoutWrapper({
         user: UserResource;
       };
 }) {
+  const [showMenu, setShowMenu] = useState(false);
+  document.addEventListener("click", () => {
+    setShowMenu(false);
+  });
+
   return (
     <div className="min-h-screen w-full bg-zinc-900 text-slate-50">
       <div className="flex min-h-screen w-full items-start justify-center">
         <div className="flex min-h-screen w-full max-w-5xl flex-col justify-between border-x border-slate-900">
-          <header className="flex items-center justify-between bg-slate-800 px-4 py-4 lg:px-8">
+          <header className="relative flex items-center justify-between bg-slate-800 px-4 py-4 lg:px-8">
             <div className="flex items-center">
               <Link
                 href="/"
@@ -49,21 +55,35 @@ export default function LayoutWrapper({
               </SignInButton>
             )}
             {user.isSignedIn && (
-              <div>
-                <Image
-                  priority
-                  src={user.user.imageUrl}
-                  alt="User Profile Image"
-                  width={50}
-                  height={50}
-                  className="rounded-full"
-                />
+              <div onClick={(e) => e.stopPropagation()}>
+                <div
+                  id="user-menu"
+                  className={
+                    !showMenu
+                      ? "absolute -bottom-[74px] right-0 z-20 bg-zinc-900 px-4 py-4 opacity-0 transition-none"
+                      : "absolute -bottom-[74px] right-0 z-20 border-[1px] border-slate-400 bg-zinc-900 px-4 py-4 opacity-100 transition-none"
+                  }
+                >
+                  <SignOutButton>
+                    <button
+                      disabled={showMenu}
+                      className="flex items-center justify-center bg-pink-700 px-6 py-2 transition-all hover:scale-105 hover:rounded-md"
+                    >
+                      Sign Out
+                    </button>
+                  </SignOutButton>
+                </div>
+                <button onClick={() => setShowMenu(!showMenu)}>
+                  <Image
+                    priority
+                    src={user.user.imageUrl}
+                    alt="User Profile Image"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                  />
+                </button>
               </div>
-              // <SignOutButton>
-              //   <button className="flex items-center justify-center bg-pink-700 px-6 py-2 transition-all hover:scale-105 hover:rounded-md">
-              //     Sign Out
-              //   </button>
-              // </SignOutButton>
             )}
           </header>
           {children}
