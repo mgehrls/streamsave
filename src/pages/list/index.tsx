5790273e7781dcfacf4b0019a4c5dfd2ccf579f6
Matt Gehrls <mattgehrls@gmail.com>
2023-12-11 17:40:32 -0500

    feature/ sort by tag

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -13,8 +13,8 @@ export default function List() {
   //create filters, sort, and view state,
   const [filters, setFilters] = useState<{
     mediaType: string;
-    tags: { id: number; name: string }[];
-  }>({ mediaType: "all", tags: [] });
+    tag: string;
+  }>({ mediaType: "all", tag: "" });
   const [sort, setSort] = useState<
     "alphaUp" | "alphaDown" | "dateAddedUp" | "dateAddedDown"
   >("alphaUp");
@@ -22,8 +22,6 @@ export default function List() {
   //pass the filtered and sorted list to the different views
 
   if (!list || !allTags) return <div>Loading...</div>;
-
-  console.log(list[0]);
 
   function isTagPresentInListItem(
     item: {
@@ -48,33 +46,33 @@ export default function List() {
       mediaId: number;
       watchLater: boolean;
     },
-    filter: { mediaType: string; tags: { id: number; name: string }[] },
+    filter: { mediaType: string; tag: string },
   ): boolean {
-    const tagsInFilter = filter.tags.map((tag) => tag.name);
     const tagsInItem = item.tags.map((tag) => tag.name);
+    const tagToFilter = filter.tag;
 
-    const commonTags = tagsInFilter.some((tag) => tagsInItem.includes(tag));
+    const commonTags = tagsInItem.includes(tagToFilter);
 
     return commonTags;
   }
 
   const filteredList = list.filter((item) => {
     if (filters.mediaType === "all") {
-      if (filters.tags.length === 0) return true;
-      if (filters.tags.length !== 0) {
+      if (filters.tag === "") return true;
+      if (filters.tag) {
         return isTagPresentInListItem(item, filters);
       }
     } else if (filters.mediaType === "movie") {
       if (item.media.type === "movie") {
-        if (filters.tags.length === 0) return true;
-        if (filters.tags.length !== 0) {
+        if (filters.tag === "") return true;
+        if (filters.tag) {
           return isTagPresentInListItem(item, filters);
         }
       }
     } else if (filters.mediaType === "tv") {
       if (item.media.type === "tv") {
-        if (filters.tags.length === 0) return true;
-        if (filters.tags.length !== 0) {
+        if (filters.tag === "") return true;
+        if (filters.tag) {
           return isTagPresentInListItem(item, filters);
         }
       }
@@ -88,26 +86,54 @@ export default function List() {
       return b.media.title.localeCompare(a.media.title);
     }
   });
-  const filterButtonClasses =
-    "hover:bg-white px-4 py-2 rounded-md hover:text-black border-[1px] border-white transition-all";
 
   return (
     <LayoutWrapper>
       <div>
         <div className="flex items-center justify-between bg-sky-600 p-4">
           <h1 className="text-xl">Your List</h1>
-          <label htmlFor="mediaFilter">Filter by Media</label>
-          <select
-            onChange={(e) =>
-              setFilters({ mediaType: e.target.value, tags: filters.tags })
-            }
-            name="mediaFilter"
-            className="text-black"
-          >
-            <option value={"all"}>All</option>
-            <option value={"tv"}>TV</option>
-            <option value={"movie"}>Movies</option>
-          </select>
+          <div className="flex flex-col gap-2">
+            <div>
+              <select
+                onChange={(e) =>
+                  setFilters({ mediaType: e.target.value, tag: filters.tag })
+                }
+                name="mediaFilter"
+                className="w-28 rounded-md bg-slate-200 px-1 py-0.5 text-xs tracking-wider text-black"
+                placeholder="Filter by Media"
+              >
+                <option value={"all"}>All</option>
+                <option value={"tv"}>TV</option>
+                <option value={"movie"}>Movies</option>
+              </select>
+            </div>
+            <div className="flex">
+              <select
+                className="w-28 rounded-md bg-slate-200 px-1 py-0.5 text-xs tracking-wider text-black"
+                id="tagFilter"
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  setFilters({
+                    mediaType: filters.mediaType,
+                    tag: e.target.value,
+                  });
+                }}
+              >
+                {allTags.tags.map((tag) => {
+                  return (
+                    <option
+                      onSelect={() => console.log(tag.name)}
+                      key={tag.id}
+                      id={tag.id.toString()}
+                      value={tag.name}
+                    >
+                      {tag.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
         </div>
         <div className="my-2 flex flex-col gap-2">
           {sortedList.map((item) => {
