1a4dc6f67c28a8a1d93307e5b0da5f32664f8f3f
Matt Gehrls <mattgehrls@gmail.com>
2024-10-18 10:28:09 -0400

    minor design tweaks

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,114 +1,42 @@
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import SearchResults from "../SearchResults";
+import { useState } from "react";
 import Image from "next/image";
 import type { User } from "~/utils/types";
 
 function Header({ user }: { user: User }) {
   const [showMenu, setShowMenu] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [showSearch, setShowSearch] = useState(false);
-  const [timer, setTimer] = useState<NodeJS.Timeout>();
-  const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-
-    const handleFocus = () => {
-      if (searchQuery !== "") {
-        setShowSearch(true);
-      } else {
-        setShowSearch(false);
-      }
-    };
-
-    const handleClick = () => {
-      setShowMenu(false);
-      setSearchQuery("");
-      setShowSearch(false);
-    };
-
-    const handleKeydown = (e: { key: string }) => {
-      if (e.key === "Escape") {
-        setShowSearch(false);
-        setShowMenu(false);
-        setSearchQuery("");
-        const searchBar = document.getElementById(
-          "search-bar",
-        ) as HTMLInputElement;
-        searchBar?.blur();
-        if (document.activeElement) {
-          (document.activeElement as HTMLInputElement).blur();
-        }
-      }
-    };
-
-    document.addEventListener("focus", handleFocus);
-    document.addEventListener("click", handleClick);
-    document.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      document.removeEventListener("focus", handleFocus);
-      document.removeEventListener("click", handleClick);
-      document.removeEventListener("keydown", handleKeydown);
-      clearTimeout(timer);
-    };
-  }, [searchQuery, timer, isClient]);
-
-  const debounce = () => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-
-    setTimer(
-      setTimeout(() => {
-        setShowSearch(true);
-      }, 1000),
-    );
-  };
-
-  const getMenuClassNames = () => {
-    if (!isClient) {
-      return "";
-    } else {
-      const menuHeight = document.getElementById("user-menu")?.clientHeight;
-      if (!menuHeight && isClient)
-        return "absolute -top-[200px] right-0 z-50 bg-zinc-900 px-4 py-4 opacity-0 transition-none";
-      return !showMenu
-        ? "absolute -top-[200px] right-0 z-50 bg-zinc-900 px-4 py-4 opacity-0 transition-none"
-        : `absolute top-[87px] flex flex-col gap-2 right-0 z-50 border-[1px] border-slate-400 bg-zinc-900 px-4 py-4 opacity-100 transition-none`;
-    }
-  };
   return (
-    <div className="flex flex-col items-center justify-center bg-[#050505]">
-      <header className="relative flex w-full max-w-5xl items-center justify-between px-4 py-4 lg:px-8">
-        <div className="flex items-center">
-          <Link
-            href="/"
-            className="flex items-end gap-1 text-xl font-bold tracking-wide"
+    <div className="flex w-full flex-col items-center justify-center bg-[#15181c]">
+      <header className="relative flex w-full max-w-5xl items-center justify-between px-4 py-4 lg:px-2">
+        <Link href="/" className="flex items-end gap-1">
+          <Image
+            priority
+            src="/images/Streamsave.svg"
+            alt="StreamSave Logo"
+            width={1900}
+            height={3330}
+            className="h-auto w-[20px]"
+          />
+          <h1
+            aria-label="Streamsave"
+            className="hidden text-xl font-bold tracking-wide sm:block"
           >
-            <Image
-              priority
-              src="/images/Streamsave.svg"
-              alt="StreamSave Logo"
-              width={1900}
-              height={3330}
-              className="h-auto w-6"
-            />
-            <h1 className="hidden sm:block">treamSave</h1>
-          </Link>
-        </div>
-        {!user.isSignedIn && (
-          <SignInButton>
-            <button className="flex items-center justify-center bg-pink-700 px-6 py-2 transition-all hover:scale-105 hover:rounded-md">
-              Sign In
-            </button>
-          </SignInButton>
-        )}
-        {user.isSignedIn && user.user && (
+            treamSave
+          </h1>
+        </Link>
+
+        {user.user && (
           <div onClick={(e) => e.stopPropagation()}>
-            <div id="user-menu" className={getMenuClassNames()}>
+            <div
+              id="user-menu"
+              className={
+                !showMenu
+                  ? "absolute -top-[200px] right-0 z-50 bg-zinc-900 px-4 py-4 opacity-0 transition-none"
+                  : `absolute right-0 top-[87px] z-50 flex flex-col gap-2 border-[1px] border-slate-400 bg-zinc-900 px-4 py-4 opacity-100 transition-none`
+              }
+            >
               <Link href="/">
                 <button className="flex items-center justify-center py-2 transition-all hover:scale-105 hover:rounded-md hover:underline">
                   Home
@@ -141,48 +69,6 @@ function Header({ user }: { user: User }) {
           </div>
         )}
       </header>
-      {user.isSignedIn && (
-        <div
-          onClick={(e) => e.stopPropagation()}
-          className="relative w-full max-w-5xl"
-        >
-          <input
-            type="text"
-            id="search-bar"
-            placeholder="Search for shows or movies..."
-            className="relative top-0 z-20 w-full bg-gray-200 px-4 py-1 text-black duration-500 lg:focus:scale-110"
-            value={searchQuery}
-            onInput={debounce}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                // window.location.assign(`/search?query=${searchQuery}`);
-              }
-            }}
-          />
-          {searchQuery !== "" && (
-            <button
-              onClick={() => {
-                setShowSearch(false);
-                setSearchQuery("");
-              }}
-              className="absolute -top-1 right-2 z-50 p-2 text-black"
-            >
-              X
-            </button>
-          )}
-
-          {showSearch && (
-            <div className="absolute left-0 top-8 z-10 mx-auto ml-2 flex max-h-96 min-w-[896] flex-col overflow-y-scroll bg-opacity-50">
-              <SearchResults
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                setShowSearch={setShowSearch}
-              />
-            </div>
-          )}
-        </div>
-      )}
     </div>
   );
 }
--- src/components/Layout/LayoutWrapper.tsx
+++ src/components/Layout/LayoutWrapper.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./Footer";
 import type { User } from "~/utils/types";
 import Header from "./Header";
+import SearchBar from "./SearchBar";
 
 export default function LayoutWrapper({
   user,
@@ -11,10 +12,13 @@ export default function LayoutWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full bg-gray-800">
+    <div className="w-full bg-[#0b0b0b]">
       <Header user={user} />
       <div className="flex justify-center">
-        <div className="max-w-5xl">{children}</div>
+        <div className="flex max-w-full flex-col lg:max-w-5xl">
+          <SearchBar />
+          {children}
+        </div>
       </div>
       <Footer />
     </div>
