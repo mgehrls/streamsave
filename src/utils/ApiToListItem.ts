125c229b1a9bbec3facc71a27580ca32282601c2
Matt Gehrls <mattgehrls@gmail.com>
2024-10-28 15:13:12 -0400

    dry logic clean up

--- src/utils/ApiToListItem.ts
+++ src/utils/ApiToListItem.ts
@@ -14,57 +14,22 @@ export function typeIsMovie(media: APIResult): media is MovieAPIResult {
 }
 
 export const SingleApiMediaToListItem : (media: APIResult | SingleMediaAPIUnity) => MongoMedia = (media) => {
-  if( typeIsExtended(media) ) {
-    if( typeIsExtendedMovie(media) ) {
-        return {
-            id: media.id,
-            title: media.title,
-            type: "movie",
-            poster: media.poster_path,
-            backdrop: media.backdrop_path,
-            description: media.overview,
-            tags: media.genres
-        }
-    }else{
-        return {
-            id: media.id,
-            title: media.name,
-            type: "tv",
-            poster: media.poster_path,
-            backdrop: media.backdrop_path,
-            description: media.overview,
-            tags: media.genres
-        }
+    const isExtended = typeIsExtended(media);
+    const isMovie = isExtended && typeIsExtendedMovie(media) || !isExtended && typeIsMovie(media);
+    return {
+        id: media.id,
+        title: isMovie ? media.title : media.name,
+        type: isMovie ? "movie" : "tv",
+        poster: media.poster_path,
+        backdrop: media.backdrop_path,
+        description: media.overview,
+        tags: isExtended ? media.genres : media.genre_ids.map((genre) => {
+          return {
+            id: genre,
+            name: genresFromAPI.find((tag) => tag.id === genre)?.name ?? "",
+          }}),
     }
-  }else if( typeIsMovie(media) ) {
-        return {
-            id: media.id,
-            title: media.title,
-            type: "movie",
-            poster: media.poster_path,
-            backdrop: media.backdrop_path,
-            description: media.overview,
-            tags: media.genre_ids.map((genre) => {
-              return {
-                id: genre,
-                name: genresFromAPI.find((tag) => tag.id === genre)?.name ?? "",
-              }})
-        }
-    }else{
-        return {
-            id: media.id,
-            title: media.name,
-            type: "tv",
-            poster: media.poster_path,
-            backdrop: media.backdrop_path,
-            description: media.overview,
-            tags: media.genre_ids.map((genre) => {
-              return {
-                id: genre,
-                name: genresFromAPI.find((tag) => tag.id === genre)?.name ?? "",
-              }})
-        }
-    }
+
   }
 
 // export const MultiApiToListItem = (mediaArray: APIResult[]) => {}
