f502495b38aa86cef11c8ed739716a0f6605c6af
Matt Gehrls <mattgehrls@gmail.com>
2024-10-29 15:13:32 -0400

    changed searchbar placement

--- src/components/Layout/LayoutWrapper.tsx
+++ src/components/Layout/LayoutWrapper.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "./Footer";
 import type { User } from "~/utils/types";
 import Header from "./Header";
-import SearchBar from "./SearchBar";
 
 export default function LayoutWrapper({
   user,
@@ -16,10 +15,7 @@ export default function LayoutWrapper({
       <div>
         <Header user={user} />
         <div className="flex h-auto justify-center">
-          <div className="flex w-full flex-col lg:max-w-5xl">
-            <SearchBar />
-            {children}
-          </div>
+          <div className="flex w-full flex-col lg:max-w-5xl">{children}</div>
         </div>
       </div>
       <Footer />
