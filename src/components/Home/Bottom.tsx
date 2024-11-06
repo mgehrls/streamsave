46567f56ad29092ba80cdb186cd5160de16b7d65
Matt Gehrls <mattgehrls@gmail.com>
2024-11-06 14:29:25 -0500

    landing finalized

--- src/components/Home/Bottom.tsx
+++ src/components/Home/Bottom.tsx
@@ -3,27 +3,42 @@ import Image from "next/image";
 
 function BottomSection() {
   return (
-    <section className="flex w-full items-center justify-center bg-gradient-to-bl from-sky-600 via-sky-900 to-zinc-900 px-12 py-4 text-white">
-      <div className="flex w-2/3 flex-col items-center justify-center gap-8">
-        <div className="flex flex-col gap-8">
-          <h2 className="line-clamp-1 text-xl font-bold leading-normal tracking-wider md:text-3xl lg:text-4xl  lg:leading-relaxed">
-            Organized How You Want
-          </h2>
-          <SignInButton mode="modal">
-            <button className="flex w-1/2 items-center justify-center bg-pink-700 px-6 py-2 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
-              Sign In
-            </button>
-          </SignInButton>
-        </div>
-      </div>
-      <div className="h-auto w-1/3">
+    <section className="flex w-full flex-col items-center justify-center bg-gradient-to-bl from-sky-600 via-sky-900 to-zinc-900 py-4 text-white md:px-12 lg:flex-row">
+      <div className="flex w-full justify-evenly lg:w-1/2">
         <Image
-          src="/images/mobile-list.png"
+          src="/images/mobile-list-view.png"
+          alt="How the user's list looks on a mobile device"
+          className="hidden py-12 md:block"
+          width={260}
+          height={336}
+        />
+        <Image
+          src="/images/mobile-card-view.png"
           alt="How the user's list looks on a mobile device"
           className="py-12"
           width={260}
           height={336}
         />
+      </div>
+      <div className="flex w-full flex-col items-center justify-center gap-8 pb-16 lg:w-1/2">
+        <div className="flex flex-col items-center gap-4 md:gap-8 lg:items-start">
+          <h2 className="line-clamp-1 text-xl font-bold leading-normal tracking-wider md:text-3xl lg:text-4xl  lg:leading-relaxed">
+            Organized How You Want
+          </h2>
+          <div className="flex items-center gap-4">
+            <SignInButton mode="modal">
+              <button className="flex items-center justify-center bg-sky-600 px-12 py-3 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
+                Sign In
+              </button>
+            </SignInButton>
+            <p>or</p>
+            <SignInButton mode="modal">
+              <button className="flex items-center justify-center bg-pink-700 px-12 py-3 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
+                Sign Up
+              </button>
+            </SignInButton>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Home/Top.tsx
+++ src/components/Home/Top.tsx
@@ -20,17 +20,25 @@ function TopSection() {
           <h1 className="flex-shrink text-3xl font-medium md:text-5xl">
             Streamsave
           </h1>
-          <SignInButton mode="modal">
-            <button className="mt-6 flex items-center justify-center bg-pink-700 px-12 py-2 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
-              Sign In
-            </button>
-          </SignInButton>
+          <p className="mt-2 w-72 leading-tight tracking-wide">
+            Organize your favorite shows and movies, or discover new ones!
+          </p>
         </div>
       </div>
       <div className="flex w-full items-center justify-center bg-gradient-to-t from-sky-700 to-transparent py-20 lg:w-1/3 lg:justify-start lg:bg-gradient-to-l lg:py-40 lg:pl-12">
-        <p className="w-48 text-center font-bold tracking-wider">
-          Organize your favorite shows and movies, or discover new ones!
-        </p>
+        <div className="flex flex-col items-center gap-2">
+          <SignInButton mode="modal">
+            <button className="flex items-center justify-center bg-sky-600 px-12 py-3 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
+              Sign In
+            </button>
+          </SignInButton>
+          <p>or</p>
+          <SignInButton mode="modal">
+            <button className="flex items-center justify-center bg-pink-700 px-12 py-3 transition-all hover:scale-105 hover:rounded-md motion-reduce:transition-none">
+              Sign Up
+            </button>
+          </SignInButton>
+        </div>
       </div>
     </section>
   );
